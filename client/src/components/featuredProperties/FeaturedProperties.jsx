89ce28818bd023894dbb057ba9a3962d538c1488
Ravi Kant Pujari <rvkantpujari@gmail.com>
2022-05-18 23:26:37 -0400

    Homepage: New Components added

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import "./featuredProperties.css";
+
+export const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/hotel/max500/103951224.jpg?k=03736dd4e1e89c1132e4957149e394d01ac6e8f64f4b09e30ade97a6176f0640&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Krakow</span>
+        <span className="fpPrice">Starting from CAD$120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/hotel/max500/103951224.jpg?k=03736dd4e1e89c1132e4957149e394d01ac6e8f64f4b09e30ade97a6176f0640&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Krakow</span>
+        <span className="fpPrice">Starting from CAD$120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/hotel/max500/103951224.jpg?k=03736dd4e1e89c1132e4957149e394d01ac6e8f64f4b09e30ade97a6176f0640&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Krakow</span>
+        <span className="fpPrice">Starting from CAD$120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
